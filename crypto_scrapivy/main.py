5269b25ea95e96f60f40753b0361ae7257e3ce07
sjihtam <none>
2020-11-10 12:20:47 +0100

    messages now well structured and transactions are checked on validity

--- crypto_scrapivy/main.py
+++ crypto_scrapivy/main.py
@@ -4,7 +4,7 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-bot_token = str(input("Please enter security token:")) 
+bot_token = str(input("Please enter security token:"))
 bot_chatID = '832555466'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
 page = "https://etherscan.io/address/0x66aeeadd49026a7cfbde0240a7b148f18966b7b7#tokentxns"
@@ -14,15 +14,22 @@ def main():
     print("init")
     recent_tx = ""
     while 1==1 :
-        temp = transaction_scraper()
-        print(temp)
+        try:
+            temp = transaction_scraper()
+            print("Old hash: " + recent_tx + "\n" + "New hash: " + temp)
+        except:
+            print("Could not fetch data\n")
         if (recent_tx != temp):
-            msg = transaction_detail_scraper(temp)
-            telegram = telegram_bot_sendtext(msg, temp)
-            print(telegram)
+            print("Found new transaction!\n")
+            try:
+                msg = transaction_detail_scraper(temp)
+                telegram = telegram_bot_sendtext(msg, temp)
+                print("Following data was send: \n")
+                print(telegram)
+            except:
+                print("Could not send data over telegram api\n")
         recent_tx = temp
-        print(recent_tx)
-        time.sleep(15)
+        time.sleep(60)
 
 def transaction_scraper():
     source = requests.get(page, headers=headers)
@@ -32,19 +39,31 @@ def transaction_scraper():
     return recent_transaction_url_stripped
 
 def transaction_detail_scraper(transaction_url):
+    #transaction_url = "/tx/0xd85247233f3b91faafe2e9cc320ba84f1671571c47132893205645b9466cfcbb" # niet succesvol
+    #transaction_url = "/tx/0xc105f1f938fc3e89b0ec549b0005714e8fc95fc276eaa3af0f73e6bddb559349" # wel succesvol
     page = "https://etherscan.io{}".format(transaction_url)
     source = requests.get(page, headers=headers)
     soup = BeautifulSoup(source.content, 'html.parser')
-    transaction_details = soup.select(".mn-3:nth-child(13) .col-md-9")
-    print(transaction_details)
-    info = str(transaction_details[0])
-    return info
+    try:
+        Token_from = str(soup.select("a.d-inline-block , .d-inline-block:nth-child(7)")[0]).split(">")[1].split("<")[0]
+        Token_from_amount = str(soup.select(".d-inline-block:nth-child(2)")[1]).split(">")[1].split("<")[0]
+        Token_to = str(soup.select(".d-inline-block:nth-child(7)")[0]).split(">")[1].split("<")[0]
+        Token_to_amount = str(soup.select(".d-inline-block~ .text-secondary+ .d-inline-block")[0]).split(">")[1].split("<")[0]
+        info = Token_from_amount + "  " + Token_from + " > " + Token_to_amount + "  " + Token_to
+        print(info)
+        return info
+    except:
+        info = "Failed transaction"
+        print(info)
+        return info
+
 #https://etherscan.io/tx/0xd64f38e6d5b0cc3cbe289761ed0ab0b610205c2a68c35fe800bc1f58451655b2
 
 def telegram_bot_sendtext(bot_message, tx_hash):
     page = "https://etherscan.io{}".format(tx_hash)
-    bot_message = 'New transaction: ' + bot_message + '\n Link: ' + page
+    bot_message = 'New transaction: ' + bot_message + ' Link: ' + page
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    print(send_text)
     response = requests.get(send_text)
     return response.json()
 
